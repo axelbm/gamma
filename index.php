344562e0ceca39ece559a6e7a8c454b5b5a1f5f1
Axel <axelbmichaud@gmail.com>
2015-11-03 15:32:12 -0500

    Firt push

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+define('WEBROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
+define('ROOT', str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']));
+
+
+echo 'HelloWorld';
+?>
+
